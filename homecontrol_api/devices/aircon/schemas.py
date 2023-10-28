821de790f05b2c2ee08bbcbd32b201b4aea36a3a
2851999 <joelvdavies@btinternet.com>
2023-10-28 20:08:42 +0100

    Add endpoints for getting and setting ac states

--- homecontrol_api/devices/aircon/schemas.py
+++ homecontrol_api/devices/aircon/schemas.py
@@ -1,3 +1,10 @@
+from typing import Optional
+
+from homecontrol_base.aircon.state import (
+    ACDeviceFanSpeed,
+    ACDeviceMode,
+    ACDeviceSwingMode,
+)
 from pydantic import BaseModel, ConfigDict, IPvAnyAddress
 
 from homecontrol_api.core.types import StringUUID
@@ -14,3 +21,35 @@ class ACDevice(BaseModel):
 class ACDevicePost(BaseModel):
     name: str
     ip_address: IPvAnyAddress
+
+
+class ACDeviceState(BaseModel):
+    # Read and write
+    power: bool
+    target_temperature: float
+    operational_mode: ACDeviceMode
+    fan_speed: ACDeviceFanSpeed
+    swing_mode: ACDeviceSwingMode
+    eco_mode: bool
+    turbo_mode: bool
+    fahrenheit: bool
+
+    # Read only
+    indoor_temperature: float
+    outdoor_temperature: float
+    display_on: bool
+
+
+class ACDeviceStatePost(BaseModel):
+    # Read and write
+    power: bool
+    target_temperature: float
+    operational_mode: ACDeviceMode
+    fan_speed: ACDeviceFanSpeed
+    swing_mode: ACDeviceSwingMode
+    eco_mode: bool
+    turbo_mode: bool
+    fahrenheit: bool
+
+    # Write only
+    prompt_tone: bool
--- homecontrol_api/routers/devices/aircon.py
+++ homecontrol_api/routers/devices/aircon.py
@@ -3,7 +3,12 @@ import json
 from fastapi import APIRouter
 from homecontrol_base import exceptions as base_exceptions
 
-from homecontrol_api.devices.aircon.schemas import ACDevice, ACDevicePost
+from homecontrol_api.devices.aircon.schemas import (
+    ACDevice,
+    ACDevicePost,
+    ACDeviceState,
+    ACDeviceStatePost,
+)
 from homecontrol_api.exceptions import DeviceNotFoundError
 from homecontrol_api.routers.dependencies import AdminUser, AnyUser, BaseService
 
@@ -16,12 +21,12 @@ aircon = APIRouter(prefix="/devices/aircon", tags=["aircon"])
 
 
 @aircon.get(path="/", summary="Get a list of all aircon devices")
-async def get_units(user: AnyUser, base_service: BaseService) -> list[ACDevice]:
+async def get_device(user: AnyUser, base_service: BaseService) -> list[ACDevice]:
     return base_service._db_conn.ac_devices.get_all()
 
 
 @aircon.post(path="/", summary="Register an air conditioning device")
-async def register_unit(
+async def register_device(
     device_info: ACDevicePost, user: AdminUser, base_service: BaseService
 ) -> ACDevice:
     try:
@@ -33,3 +38,20 @@ async def register_unit(
         ).get_info()
     except base_exceptions.DeviceNotFoundError as exc:
         raise DeviceNotFoundError(str(exc)) from exc
+
+
+@aircon.get(path="/{device_id}/state")
+async def get_device_state(
+    device_id: str, user: AnyUser, base_service: BaseService
+) -> ACDeviceState:
+    device = await base_service.aircon.get_device(device_id=device_id)
+    return await device.get_state()
+
+
+@aircon.post(path="/{device_id}/state")
+async def post_device_state(
+    device_id: str, state: ACDeviceStatePost, user: AnyUser, base_service: BaseService
+) -> ACDeviceState:
+    device = await base_service.aircon.get_device(device_id=device_id)
+    await device.set_state(state=state)
+    return await device.get_state()
